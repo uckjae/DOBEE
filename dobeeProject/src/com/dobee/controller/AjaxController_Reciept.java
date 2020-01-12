4b907732e99c1914725beb515f04978b41d600bb
kospsh157 <kospsh157@gmail.com>
2020-01-12 16:08:54 +0900

    비용/ 사진 업로드 수정  컨트롤러 do 에서 ajax 로 변경 / 성호

--- dobeeProject/src/com/dobee/controller/AjaxController_Reciept.java
+++ dobeeProject/src/com/dobee/controller/AjaxController_Reciept.java
@@ -18,43 +18,51 @@ public class AjaxController_Reciept {
 	private ReceiptService receiptService;
 	
 	
-	@RequestMapping(value="/fileUploadAjax.do", method=RequestMethod.GET)
-	public ModelAndView fileUploadAjaxForm() {
-		
+
+	
+	
+	
+	@RequestMapping(value="/fileUploadAjax.ajax", method=RequestMethod.POST)
+	public ModelAndView fileUploadAjax(MultipartHttpServletRequest mRequest) {
+		//System.out.println("dd");
 		ModelAndView mav = new ModelAndView();
-		
-		mav.setViewName("board/fileUpload_ajax");
-		
+		if(receiptService.fileUpload(mRequest)) {
+			mav.addObject("result", "success");
+		} else {
+			mav.addObject("result", "fail");
+		}
+		mav.setViewName("jsonView"); //view �̸� : json ���
 		return mav;
 	}
 	
-	@RequestMapping(value="/fileUploadAjax.do", method=RequestMethod.POST)
-	public ModelAndView fileUploadAjax(MultipartHttpServletRequest mRequest) {
-		System.out.println("컨트롤단/boardControlelr.java :  fileUploadAjax.do 요청들어왔다 ");
-		ArrayList<String> arrayList = new ArrayList<>();
-		
-		ModelAndView mav = new ModelAndView();
-		arrayList = receiptService.fileUpload(mRequest);
-		try {
-			System.out.println("컨트롤단/boardControlelr.java :try 구문 시작");
-			if(arrayList.get(0).equals("true")) {
-				mav.addObject("result", "success");
-				mav.addObject("uploadPath", arrayList.get(1));
-				mav.addObject("saveFileName", arrayList.get(2));
-			} else {
-				System.out.println("try 구문 else 에 빠짐 ");
-				mav.addObject("result", "fail");
-				
-			}
-			mav.setViewName("jsonView");
-			
-		}catch (Exception e) {
-			System.out.println(e);
-			System.out.println("컨트롤단/boardControlelr.java :  try 문에서 예외발생");
-		}
-		
-		return mav;
-	}
+	
+//	@RequestMapping(value="/fileUploadAjax.do", method=RequestMethod.POST)
+//	public ModelAndView fileUploadAjax(MultipartHttpServletRequest mRequest) {
+//		System.out.println("컨트롤단/boardControlelr.java :  fileUploadAjax.do 요청들어왔다 ");
+//		ArrayList<String> arrayList = new ArrayList<>();
+//		
+//		ModelAndView mav = new ModelAndView();
+//		arrayList = receiptService.fileUpload(mRequest);
+//		try {
+//			System.out.println("컨트롤단/boardControlelr.java :try 구문 시작");
+//			if(arrayList.get(0).equals("true")) {
+//				mav.addObject("result", "success");
+//				mav.addObject("uploadPath", arrayList.get(1));
+//				mav.addObject("saveFileName", arrayList.get(2));
+//			} else {
+//				System.out.println("try 구문 else 에 빠짐 ");
+//				mav.addObject("result", "fail");
+//				
+//			}
+//			mav.setViewName("jsonView");
+//			
+//		}catch (Exception e) {
+//			System.out.println(e);
+//			System.out.println("컨트롤단/boardControlelr.java :  try 문에서 예외발생");
+//		}
+//		
+//		return mav;
+//	}
 	
 	
 }
--- dobeeProject/src/com/dobee/services/ReceiptService.java
+++ dobeeProject/src/com/dobee/services/ReceiptService.java
@@ -14,12 +14,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class ReceiptService {
 	
 	
-	public ArrayList<String> fileUpload(MultipartHttpServletRequest mRequest) {
+	public boolean fileUpload(MultipartHttpServletRequest mRequest) {
 
-		ArrayList<String> arrayList = new ArrayList<>();
 		
-			
-		String isSuccess = "false";
+boolean isSuccess = false;
 		
 		String uploadPath = "/users/psh/file/";
 		
@@ -28,15 +26,15 @@ public class ReceiptService {
 		if (!dir.isDirectory()) {
 			dir.mkdirs();
 		}
-		Iterator<String> iter = mRequest.getFileNames();
 		
+		Iterator<String> iter = mRequest.getFileNames();
 		while(iter.hasNext()) {
-			System.out.println("서비스단 / ReceiptService:  와일문 시작 ");
 			String uploadFileName = iter.next();
+			
 			MultipartFile mFile = mRequest.getFile(uploadFileName);
 			String originalFileName = mFile.getOriginalFilename();
 			String saveFileName = originalFileName;
-			System.out.println("upload 서비스단 시작");
+			
 			if(saveFileName != null && !saveFileName.equals("")) {
 				if(new File(uploadPath + saveFileName).exists()) {
 					saveFileName = saveFileName + "_" + System.currentTimeMillis();
@@ -44,31 +42,73 @@ public class ReceiptService {
 				
 				try {
 					mFile.transferTo(new File(uploadPath + saveFileName));
-					isSuccess = "true";				
+					isSuccess = true;				
 				} catch (IllegalStateException e) {
 					e.printStackTrace();
-					System.out.println("서비스단 / boardService.java: try 구문 예외 발생");
-					isSuccess = "false";
+					isSuccess = false;
 				} catch (IOException e) {
 					e.printStackTrace();
-					System.out.println("서비스단 / boardService.java: try 구문 예외 발생222");
-					isSuccess = "false";
+					isSuccess = false;
 				}
-				
 			} // if end
-			else {
-				System.out.println("서비스단 / ReceiptService: form 태그로 부터 온 데이타가 없습니다.");
-		
-			}
-		
-			arrayList.add(isSuccess);
-			arrayList.add(uploadPath);
-			arrayList.add(saveFileName);
-			System.out.println(uploadPath);
-			System.out.println(saveFileName);
-			System.out.println(isSuccess);
 		} // while end
-		return arrayList;
+		return isSuccess;
+		
+		
+		
+//		ArrayList<String> arrayList = new ArrayList<>();
+//		
+//			
+//		String isSuccess = "false";
+//		
+//		String uploadPath = "/users/psh/file/";
+//		
+//		File dir = new File(uploadPath);
+//
+//		if (!dir.isDirectory()) {
+//			dir.mkdirs();
+//		}
+//		Iterator<String> iter = mRequest.getFileNames();
+//		
+//		while(iter.hasNext()) {
+//			System.out.println("서비스단 / ReceiptService:  와일문 시작 ");
+//			String uploadFileName = iter.next();
+//			MultipartFile mFile = mRequest.getFile(uploadFileName);
+//			String originalFileName = mFile.getOriginalFilename();
+//			String saveFileName = originalFileName;
+//			System.out.println("upload 서비스단 시작");
+//			if(saveFileName != null && !saveFileName.equals("")) {
+//				if(new File(uploadPath + saveFileName).exists()) {
+//					saveFileName = saveFileName + "_" + System.currentTimeMillis();
+//				}
+//				
+//				try {
+//					mFile.transferTo(new File(uploadPath + saveFileName));
+//					isSuccess = "true";				
+//				} catch (IllegalStateException e) {
+//					e.printStackTrace();
+//					System.out.println("서비스단 / boardService.java: try 구문 예외 발생");
+//					isSuccess = "false";
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//					System.out.println("서비스단 / boardService.java: try 구문 예외 발생222");
+//					isSuccess = "false";
+//				}
+//				
+//			} // if end
+//			else {
+//				System.out.println("서비스단 / ReceiptService: form 태그로 부터 온 데이타가 없습니다.");
+//		
+//			}
+//		
+//			arrayList.add(isSuccess);
+//			arrayList.add(uploadPath);
+//			arrayList.add(saveFileName);
+//			System.out.println(uploadPath);
+//			System.out.println(saveFileName);
+//			System.out.println(isSuccess);
+//		} // while end
+//		return arrayList;
 	} // fileUpload end
 
 }
