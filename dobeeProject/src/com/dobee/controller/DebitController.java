e57e9b8c2b1f0334b4b51ce331d49cb220e50375
kospsh157 <kospsh157@gmail.com>
2020-01-17 14:54:54 +0900

    no message

--- dobeeProject/src/com/dobee/controller/DebitController.java
+++ dobeeProject/src/com/dobee/controller/DebitController.java
@@ -1,6 +1,6 @@
 package com.dobee.controller;
 
-import java.util.Date;
+import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
